6c4286b58d20634e8bf63b7ae90ab2f070749b2c
jaehyeon(wiz) <eiirwp@gmail.com>
2022-01-05 21:35:58 +0900

    add map in map page with proper size

--- src/components/pages/MapPage.tsx
+++ src/components/pages/MapPage.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { TemplateBasic } from '../templates/TemplateBasic';
 import styled from 'styled-components';
 
 export type MapPageProps = {
 }
 
+const SIDE_BAR_WIDTH = 320
+
 const MapPageDiv = styled.div`
 	width: 100%;
 	height: 100%;
+	flex-direction: row;
 `;
 
 const SideBarWrapper = styled.div`
@@ -15,18 +18,31 @@ const SideBarWrapper = styled.div`
 `;
 
 const SideBarDiv = styled.div`
-	
+	width: ${SIDE_BAR_WIDTH}px;
 `;
 
 const MapWrapper = styled.div`
-	
+	width: calc(100% - ${SIDE_BAR_WIDTH}px);
+	height: 100%;
 `;
 
 const MapDiv = styled.div`
-	
+	width: 100%;
+	height: 100%;
 `;
 
 export const MapPage:React.FunctionComponent<MapPageProps> = () => {
+	
+	useEffect(() => {
+		if (!naver) return;
+
+		const mapOption = {
+			center: new naver.maps.LatLng(37.3595704, 127.105399),
+			zoom: 10
+		}
+		const map = new naver.maps.Map("mainMap", mapOption);
+	}, [])
+
 	return (
 		<TemplateBasic>
 			<MapPageDiv>
@@ -37,9 +53,7 @@ export const MapPage:React.FunctionComponent<MapPageProps> = () => {
 				</SideBarWrapper>
 
 				<MapWrapper>
-					<MapDiv>
-						맵
-					</MapDiv>
+					<MapDiv id={"mainMap"}/>
 				</MapWrapper>
 			</MapPageDiv>
 		</TemplateBasic>
