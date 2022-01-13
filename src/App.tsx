aa7224b0019dc8cb824b4d38996f26d7914bc227
ssssksss <ssssksss@gmail.com>
2022-01-13 21:56:11 +0900

    add login,signup ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components'
 
 import { HomePage } from './components/pages/HomePage';
@@ -11,25 +11,25 @@ import { BrowserRouter } from 'react-router-dom';
 import { GlobalStyle } from './styles/GlobalStyle';
 import { themes } from './styles/theme';
 import { Provider as ReduxProvider } from 'react-redux';
-import {store} from './store';
+import { store } from './store';
 
 const App: React.FunctionComponent = () => {
-  return (
-	<ReduxProvider store={store}>
-		<ThemeProvider theme={themes.light}>
-			<GlobalStyle/>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<HomePage/>}/>
-					<Route path="/login" element={<LoginPage />}/>
-					<Route path="/signup" element={<SignupPage />}/>
-					<Route path="/map" element={<MapPage />}/>
-					<Route path="/*" element={<ErrorPage />}/>
-				</Routes> 
-			</BrowserRouter>
-		</ThemeProvider>
-	</ReduxProvider>
-  );
+	return (
+		<ReduxProvider store={store}>
+			<ThemeProvider theme={themes.light}>
+				<GlobalStyle />
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<HomePage />} />
+						<Route path="/login" element={<LoginPage />} />
+						<Route path="/signup" element={<SignupPage />} />
+						<Route path="/map" element={<MapPage />} />
+						<Route path="/*" element={<ErrorPage />} />
+					</Routes>
+				</BrowserRouter>
+			</ThemeProvider>
+		</ReduxProvider>
+	);
 }
 
 export default App;
