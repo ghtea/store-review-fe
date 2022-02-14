d219205826f64218aadb250987a87b39a43a5d03
ghtea <eiirwp@gmail.com>
2022-02-14 22:40:21 +0900

    fix crypto js import

--- src/utils/crypto/index.ts
+++ src/utils/crypto/index.ts
@@ -1,3 +1,5 @@
+import CryptoJS from "crypto-js"
+
 export const decryptAes = (value: string) => {
   const key = CryptoJS.enc.Utf8.parse( process.env.REACT_APP_AES_KEY || "" );
   const iv  = CryptoJS.enc.Utf8.parse( process.env.REACT_APP_AES_IV || "" );
